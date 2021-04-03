98f464ad8339ac43c7721b4cf7864295edaaf167
Robert Hammond <hamrobe502@gmail.com>
2021-04-03 16:30:53 -0500

    added main.py

--- lastfm.py
+++ lastfm.py
@@ -5,31 +5,26 @@ import pylast
 	
 API_KEY = "630a67efa81819f816483ae706932202"
 API_SECRET = "011f1c67ae96efa46fdeb854dad3950c"
-USERNAME = "lynmarie44"
-PASSWORD = ""
 
 # Type help(pylast.LastFMNetwork) or help(pylast) in a Python interpreter
 # to get more help about anything and see examples of how it works
+# https://www.last.fm/api
 
 
-if __name__  == "__main__":
-
+def get_network(username, password_hash=None):
     # In order to perform a write operation you need to authenticate yourself
     # password_hash = pylast.md5(PASSWORD)
-    # print(password_hash)
-
-    network = pylast.LastFMNetwork(
+    if password_hash:
+        return pylast.LastFMNetwork(
+            api_key=API_KEY,
+            api_secret=API_KEY,
+            username=username,
+            password_hash=password_hash,
+        )
+    return pylast.LastFMNetwork(
         api_key=API_KEY,
-        api_secret=API_SECRET,
-        username=USERNAME,
-        # password_hash=password_hash,
+        api_secret=API_KEY,
+        username=username,
     )
 
-    name = input("last.fm username: ")
-    usr = network.get_user(name)
-    top_tracks = usr.get_top_tracks(period=pylast.PERIOD_12MONTHS)
-    n = min(10, len(top_tracks))
 
-    print(f"{name}'s top 10 tracks:")
-    for i in range(n):
-        print(f"{i+1:2d}: {top_tracks[i].item}")
