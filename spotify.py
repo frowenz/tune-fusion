303ea966421543a1ec992fc3fabb76b506a1b719
Robert Hammond <hamrobe502@gmail.com>
2021-04-03 22:38:13 -0500

    first draft of shared playlist algorithm

--- spotify.py
+++ spotify.py
@@ -1,37 +1,58 @@
-from bottle import request
 import spotipy
-from spotipy import oauth2
+import spotipy.util as util
+from spotipy.oauth2 import SpotifyClientCredentials
 import os
+import pandas as pd
 
 SPOTIPY_CLIENT_ID = "21732af26e344c0ca09a681b2cde2253"
-SPOTIPY_CLIENT_SECRET  = "2ebe7cced83d43698a5dcc994d274d57"
+SPOTIPY_CLIENT_SECRET  = "3cd6b42776f8489cb18bea190d7d98f5"
+# os.environ['SPOTIPY_CLIENT_ID'] = SPOTIPY_CLIENT_ID
+# os.environ['SPOTIPY_CLIENT_SECRET'] = SPOTIPY_CLIENT_SECRET
 
-PORT_NUMBER = 8080
-SPOTIPY_REDIRECT_URI = 'http://localhost:8080'
-SCOPE = 'user-library-read'
-CACHE = '.spotipyoauthcache'
+spotify = spotipy.Spotify(client_credentials_manager=SpotifyClientCredentials(client_id=SPOTIPY_CLIENT_ID, client_secret=SPOTIPY_CLIENT_SECRET))
 
-sp_oauth = oauth2.SpotifyOAuth( SPOTIPY_CLIENT_ID, SPOTIPY_CLIENT_SECRET,SPOTIPY_REDIRECT_URI,scope=SCOPE,cache_path=CACHE )
+def get_song_info(artist, song):
+    # assume that the first result is the correct song
+    results = spotify.search(q=f'{artist} {song}', limit=1, type="track")
+    item = results['tracks']['items'][0]
+    artist_uri = item['artists'][0]['uri'] # just go with the first artist if there are multiple
+    song_uri = item['uri']
+    duration_ms = item['duration_ms']
+    explicit = item['explicit']
+    album = item['album']['name']
+    popularity = item['popularity']
+    return artist, artist_uri, song, song_uri, duration_ms, explicit, album, popularity
 
+def add_features(user1_songs, user2_songs):
+    user1_list = []
+    for song in user1_songs['song_uri']:
+        row = pd.DataFrame(spotify.audio_features(tracks=[song]))
+        user1_list.append(row)
+    user1_df = pd.concat(user1_list)
 
-access_token = ""
+    user2_list = []
+    for song in user2_songs['song_uri']:
+        row = pd.DataFrame(spotify.audio_features(tracks=[song]))
+        user2_list.append(row)
+    user2_df = pd.concat(user2_list)
 
-token_info = sp_oauth.get_cached_token()
+    dfs = [user1_df, user2_df]
+    dfs = pd.concat(dfs)
+    # drop unnecessary features
+    dfs.drop(['type','track_href','analysis_url','time_signature','duration_ms','uri','instrumentalness','liveness','loudness','key','mode'],1,inplace=True)
+    dfs.set_index('id',inplace=True)
+    return dfs
 
-if token_info:
-    print("Found cached token!")
-    access_token = token_info['access_token']
-else:
-    url = request.url
-    code = sp_oauth.parse_response_code(url)
-    if code != url:
-        print("Found Spotify auth code in Request URL! Trying to get valid access token...")
-        token_info = sp_oauth.get_access_token(code)
-        access_token = token_info['access_token']
-
-
-# def get_song(artist, song_title):
-#     # song_title = song_title.replace(' ', '%20')
-#     # artist = artist.replace(' ', '%20')
-#     results = spotify.search(q=f'track:{song_title}%20artist:{artist}', limit=3, type="track")
-#     print(results)
+def get_recs(list_of_recs, adj_list_of_recs):
+    #Getting 1 recommended song from each cluster with less than 4 songs, 2 recommended songs from each cluster with 4-5 songs
+    list_of_recommendations = [0]*len(list_of_recs)
+    k = 0
+    list_of_recommendations = [0]*len(list_of_recs)
+    while k < len(list_of_recs):
+        if len(adj_list_of_recs[k]) < 4:
+            list_of_recommendations[k] = spotify.recommendations(seed_tracks=adj_list_of_recs[k],limit=1)
+        else:
+            list_of_recommendations[k] = spotify.recommendations(seed_tracks=adj_list_of_recs[k],limit=2)
+        k += 1
+    
+    return list_of_recommendations
